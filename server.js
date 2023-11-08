733e01a5f89937dc75a4b89f4a311efa3615b9c3
ebarroso24 <134114010+ebarroso24@users.noreply.github.com>
2023-11-08 10:27:50 -0500

    coding for connecting routes added

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes  = require("./routes/htmlRoutes");
 
 const PORT = process.env.port || 3001;
 
@@ -9,8 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 
-app.listen(PORT, () =>
+app.listen(PORT, () =>{
   console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+});
